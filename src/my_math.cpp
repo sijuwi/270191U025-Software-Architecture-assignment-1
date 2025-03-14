a21423f08611548bc5277948ea19c58b15ea6d0a
sijuwi <simonjuulwinther@gmail.com>
2025-03-14 08:23:11 +0100

    ændringer

--- src/my_math.cpp
+++ src/my_math.cpp
@@ -2,8 +2,8 @@
 
 int sum(int a, int b)
 {
-    // int unsafe[10];
-    // unsafe[10] = 1;
-    // int* leak = (int*)malloc(sizeof(int));
+   int unsafe[10]; // int unsafe[10];
+   unsafe[9]=1; // unsafe[10] = 1;
+   int* leak =(int*)malloc(sizeof(int)); // int* leak = (int*)malloc(sizeof(int));
     return a + b;
 }
